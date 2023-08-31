17274cd92dff19c0f8d2c729a796832fcfc42418
Jayson <kanekiken101017@gmail.com>
2023-08-31 15:59:44 +0800

    initial commit

---
+++ android/src/com/vvv/gamebipbop/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.vvv.gamebipbop;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.vvv.gamebipbop.BipBop;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		initialize(new BipBop(), config);
+	}
+}
